b46074903a17e91baa0f9ddab9c2b24595d15765
olegfomenko2002 <oleg.fomenko2002@gmail.com>
2024-02-05 15:22:53 +0200

    adding Weighted Norm Linear Argument from paper

---
+++ go/bpp/docs_test.go
@@ -0,0 +1,112 @@
+package bpp
+
+import (
+	"bytes"
+	"fmt"
+	"math/big"
+	"testing"
+
+	"github.com/cloudflare/bn256"
+)
+
+func TestWNLA(t *testing.T) {
+	// Public
+	const N = 4
+
+	g := points(1)[0]
+	G := points(N)
+
+	H := points(N)
+
+	c := values(N)
+
+	ro := values(1)[0]
+	mu := mul(ro, ro)
+	roinv := new(big.Int).ModInverse(ro, bn256.Order)
+
+	// Private
+	l := []*big.Int{big.NewInt(4), big.NewInt(5), big.NewInt(10), big.NewInt(1)}
+	n := []*big.Int{big.NewInt(2), big.NewInt(1), big.NewInt(2), big.NewInt(10)}
+
+	// Com
+	v := add(vectorMul(c, l), wightVectorMul(n, n, mu))
+	C := new(bn256.G1).ScalarMult(g, v)
+	C.Add(C, vectorPointScalarMul(H, l))
+	C.Add(C, vectorPointScalarMul(G, n))
+
+	for {
+		if len(l)+len(n) < 6 {
+
+			// Prover sends l, n to Verifier
+			// Next verifier computes:
+			_v := add(vectorMul(c, l), wightVectorMul(n, n, mu))
+
+			_C := new(bn256.G1).ScalarMult(g, _v)
+			_C.Add(_C, vectorPointScalarMul(H, l))
+			_C.Add(_C, vectorPointScalarMul(G, n))
+
+			if !bytes.Equal(_C.Marshal(), C.Marshal()) {
+				panic("Failed to verify!")
+			}
+
+			fmt.Println("Verified!")
+			return
+		}
+
+		// Verifier selects random challenge
+		y := values(1)[0]
+
+		// Prover calculates new reduced values, vx and vr and sends X, R to verifier
+		c0, c1 := reduceVector(c)
+		l0, l1 := reduceVector(l)
+		n0, n1 := reduceVector(n)
+		G0, G1 := reducePoints(G)
+		H0, H1 := reducePoints(H)
+
+		l_ := vectorAdd(l0, vectorMulOnScalar(l1, y))
+		n_ := vectorAdd(vectorMulOnScalar(n0, roinv), vectorMulOnScalar(n1, y))
+
+		//v_ := add(vectorMul(c_, l_), wightVectorMul(n_, n_, mul(mu, mu)))
+
+		vx := add(
+			mul(wightVectorMul(n0, n1, mul(mu, mu)), mul(big.NewInt(2), roinv)),
+			add(vectorMul(c0, l1), vectorMul(c1, l0)),
+		)
+
+		vr := add(wightVectorMul(n1, n1, mul(mu, mu)), vectorMul(c1, l1))
+
+		X := new(bn256.G1).ScalarMult(g, vx)
+		X.Add(X, vectorPointScalarMul(H0, l1))
+		X.Add(X, vectorPointScalarMul(H1, l0))
+		X.Add(X, vectorPointScalarMul(G0, vectorMulOnScalar(n1, ro)))
+		X.Add(X, vectorPointScalarMul(G1, vectorMulOnScalar(n0, roinv)))
+
+		R := new(bn256.G1).ScalarMult(g, vr)
+		R.Add(R, vectorPointScalarMul(H1, l1))
+		R.Add(R, vectorPointScalarMul(G1, n1))
+
+		// Submit R, X to Verifier
+
+		// Both computes
+		H_ := vectorPointsAdd(H0, vectorPointMulOnScalar(H1, y))
+		G_ := vectorPointsAdd(vectorPointMulOnScalar(G0, ro), vectorPointMulOnScalar(G1, y))
+		c_ := vectorAdd(c0, vectorMulOnScalar(c1, y))
+
+		ro_ := mu
+		mu_ := mul(mu, mu)
+
+		C_ := new(bn256.G1).Set(C)
+		C_.Add(C_, new(bn256.G1).ScalarMult(X, y))
+		C_.Add(C_, new(bn256.G1).ScalarMult(R, sub(mul(y, y), big.NewInt(1))))
+
+		// Recursive run
+		G = G_
+		H = H_
+		c = c_
+		C = C_
+		ro = ro_
+		mu = mu_
+		l = l_
+		n = n_
+	}
+}
---
+++ go/bpp/util.go
@@ -0,0 +1,170 @@
+package bpp
+
+import (
+	"crypto/rand"
+	"math/big"
+
+	"github.com/cloudflare/bn256"
+)
+
+func values(n int) []*big.Int {
+	res := make([]*big.Int, n)
+	var err error
+
+	for i := range res {
+		res[i], err = rand.Int(rand.Reader, bn256.Order)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	return res
+}
+
+func ones(n int) []*big.Int {
+	res := make([]*big.Int, n)
+	for i := range res {
+		res[i] = big.NewInt(1)
+	}
+	return res
+}
+
+func zeros(n int) []*big.Int {
+	res := make([]*big.Int, n)
+	for i := range res {
+		res[i] = big.NewInt(0)
+	}
+	return res
+}
+
+func points(n int) []*bn256.G1 {
+	res := make([]*bn256.G1, n)
+	var err error
+	for i := range res {
+		if _, res[i], err = bn256.RandomG1(rand.Reader); err != nil {
+			panic(err)
+		}
+	}
+	return res
+}
+
+func add(x *big.Int, y *big.Int) *big.Int {
+	return new(big.Int).Mod(new(big.Int).Add(x, y), bn256.Order)
+}
+
+func sub(x *big.Int, y *big.Int) *big.Int {
+	return new(big.Int).Mod(new(big.Int).Sub(x, y), bn256.Order)
+}
+
+func mul(x *big.Int, y *big.Int) *big.Int {
+	return new(big.Int).Mod(new(big.Int).Mul(x, y), bn256.Order)
+}
+
+func vectorMul(a []*big.Int, b []*big.Int) *big.Int {
+	if len(b) != len(a) {
+		panic("invalid length")
+	}
+
+	res := big.NewInt(0)
+	for i := 0; i < len(a); i++ {
+		res = add(res, mul(a[i], b[i]))
+	}
+	return res
+}
+
+func wightVectorMul(a []*big.Int, b []*big.Int, mu *big.Int) *big.Int {
+	if len(b) != len(a) {
+		panic("invalid length")
+	}
+
+	res := big.NewInt(0)
+	step := new(big.Int).Set(mu)
+	for i := 0; i < len(a); i++ {
+		res = add(res, mul(mul(a[i], b[i]), step))
+		step = mul(step, mu)
+	}
+	return res
+}
+
+func vectorPointScalarMul(g []*bn256.G1, a []*big.Int) *bn256.G1 {
+	if len(g) != len(a) {
+		panic("invalid length for scalar mul")
+	}
+
+	res := new(bn256.G1).ScalarMult(g[0], a[0])
+	for i := 1; i < len(g); i++ {
+		res.Add(res, new(bn256.G1).ScalarMult(g[i], a[i]))
+	}
+	return res
+}
+
+func reduceVector(v []*big.Int) ([]*big.Int, []*big.Int) {
+	res0 := make([]*big.Int, 0, len(v)/2)
+	res1 := make([]*big.Int, 0, len(v)/2)
+
+	for i := range v {
+		if i%2 == 0 {
+			res0 = append(res0, v[i])
+		} else {
+			res1 = append(res1, v[i])
+		}
+	}
+
+	return res0, res1
+}
+
+func reducePoints(v []*bn256.G1) ([]*bn256.G1, []*bn256.G1) {
+	res0 := make([]*bn256.G1, 0, len(v)/2)
+	res1 := make([]*bn256.G1, 0, len(v)/2)
+
+	for i := range v {
+		if i%2 == 0 {
+			res0 = append(res0, v[i])
+		} else {
+			res1 = append(res1, v[i])
+		}
+	}
+
+	return res0, res1
+}
+
+func vectorPointMulOnScalar(g []*bn256.G1, a *big.Int) []*bn256.G1 {
+	res := make([]*bn256.G1, len(g))
+	for i := range res {
+		res[i] = new(bn256.G1).ScalarMult(g[i], a)
+	}
+	return res
+}
+
+func vectorMulOnScalar(a []*big.Int, c *big.Int) []*big.Int {
+	res := make([]*big.Int, len(a))
+	for i := range res {
+		res[i] = mul(a[i], c)
+	}
+	return res
+}
+
+func vectorAdd(a []*big.Int, b []*big.Int) []*big.Int {
+	if len(b) != len(a) {
+		panic("invalid length")
+	}
+
+	res := make([]*big.Int, len(a))
+	for i := 0; i < len(res); i++ {
+		res[i] = add(a[i], b[i])
+	}
+
+	return res
+}
+
+func vectorPointsAdd(a, b []*bn256.G1) []*bn256.G1 {
+	if len(a) != len(b) {
+		panic("invalid length for scalar mul")
+	}
+
+	res := make([]*bn256.G1, len(a))
+	for i := range res {
+		res[i] = new(bn256.G1).Add(a[i], b[i])
+	}
+	return res
+}
