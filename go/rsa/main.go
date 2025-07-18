958d7992fdad40358f4b835831e0292a4a0a4fb0
olegfomenko <oleg.fomenko2002@gmail.com>
2025-07-19 01:54:26 +0300

    fixed rsa

--- go/rsa/main.go
+++ go/rsa/main.go
@@ -5,17 +5,19 @@
 package rsa
 
 import (
-	"crypto/rand"
 	"math/big"
 
 	"github.com/olegfomenko/crypto/go/math"
 )
 
-const size = 128
+const size = 256
+
+const Exp = 65537
+
+var e = big.NewInt(Exp)
 
 type PublicKey struct {
 	n *big.Int
-	e *big.Int
 }
 
 type PrivateKey struct {
@@ -41,27 +43,12 @@ func GeneratePrivateKey() (*PrivateKey, error) {
 	// phi(n) = (p - 1)(q - 1) because p and q - primes
 	phiN := new(big.Int).Mul(new(big.Int).Sub(p, big.NewInt(1)), new(big.Int).Sub(q, big.NewInt(1)))
 
-	e := big.NewInt(1)
-
-	// in most cases will be only one iteration, but...
-	for {
-		e, err = rand.Int(rand.Reader, phiN)
-		if err != nil {
-			return nil, err
-		}
-
-		if math.GCD(e, phiN).Cmp(big.NewInt(1)) == 0 {
-			break
-		}
-	}
-
 	// Euler's theorem can be used
 	d := new(big.Int).ModInverse(e, phiN)
 
 	return &PrivateKey{
 		PublicKey: &PublicKey{
 			n: n,
-			e: e,
 		},
 		p:    p,
 		q:    q,
@@ -71,7 +58,7 @@ func GeneratePrivateKey() (*PrivateKey, error) {
 }
 
 func Encrypt(msg *big.Int, pk *PublicKey) *big.Int {
-	return new(big.Int).Exp(msg, pk.e, pk.n)
+	return new(big.Int).Exp(msg, e, pk.n)
 }
 
 func Decrypt(cypher *big.Int, prv *PrivateKey) *big.Int {
